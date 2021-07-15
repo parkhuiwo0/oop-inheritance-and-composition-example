08a0246faa95a4b1acc5e48ba24210cc83ef0c41
휴/박희우 <parkhuiwo0@gmail.com>
2021-07-15 17:54:37 +0900

    결제 내역 리스트의 총 금액을 계산하는 책임을 할당하라.

--- src/domain/card/Card.java
+++ src/domain/card/Card.java
@@ -1,5 +1,6 @@
 package domain.card;
 
+import domain.money.Money;
 import domain.payment.Payment;
 
 import java.util.ArrayList;
@@ -14,6 +15,20 @@ public class Card {
 
     public List<Payment> getPayments() {
         return payments;
+    }
+
+    /**
+     * 결제 내역을 순회 하면서 금액을 더합니다.
+     * @return 결제 내역의 총 금액 합산
+     */
+    public Money calculateFee() {
+        Money fee = Money.ZERO;
+
+        for (Payment payment : payments) {
+            fee.plus(payment.getFee());
+        }
+
+        return fee;
     }
 
 }
