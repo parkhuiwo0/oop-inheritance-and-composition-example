191590a6a067472be7029cd58c44f46a3070cd37
parkhuiwo0 <huiwo0@naver.com>
2021-07-15 22:56:46 +0900

    코드 재사용을 위해 Card 클래스를 상속시켜라

--- src/domain/card/FrequencyDiscountCard.java
+++ src/domain/card/FrequencyDiscountCard.java
@@ -3,41 +3,56 @@ package domain.card;
 import domain.money.Money;
 import domain.payment.Payment;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class FrequencyDiscountCard {
+public class FrequencyDiscountCard extends Card {
     private static final int TARGET_DISCOUNT_FREQUENCY = 10;
     private static final Money discountAmount = Money.won(500);
 
-    private List<Payment> payments = new ArrayList<>();
-
     public FrequencyDiscountCard(List<Payment> payments) {
-        this.payments = payments;
+        super(payments);
     }
 
-    public List<Payment> getPayments() {
-        return payments;
-    }
-
-    /**
-     * 결제 내역을 순회 하면서 금액을 더합니다.
-     * 11번째 결제 내역부터는 500원씩 할인합니다.
-     * @return 결제 내역의 총 금액 합산
-     */
+    @Override
     public Money calculateFee() {
-        Money fee = Money.ZERO;
-        int count = 0;
+        Money fee = super.calculateFee();
+        Money discountFee = Money.ZERO;
 
-        for (Payment payment : payments) {
-            fee.plus(payment.getFee());
-
-            if (++count > TARGET_DISCOUNT_FREQUENCY) {
-                fee.minus(discountAmount);
-            }
+        if (getPayments().size() > TARGET_DISCOUNT_FREQUENCY) {
+            discountFee = Money.won(((getPayments().size() - TARGET_DISCOUNT_FREQUENCY) * 500));
         }
 
-        return fee;
+        return fee.minus(discountFee);
     }
+
+    //    private List<Payment> payments = new ArrayList<>();
+//
+//    public FrequencyDiscountCard(List<Payment> payments) {
+//        this.payments = payments;
+//    }
+//
+//    public List<Payment> getPayments() {
+//        return payments;
+//    }
+//
+//    /**
+//     * 결제 내역을 순회 하면서 금액을 더합니다.
+//     * 11번째 결제 내역부터는 500원씩 할인합니다.
+//     * @return 결제 내역의 총 금액 합산
+//     */
+//    public Money calculateFee() {
+//        Money fee = Money.ZERO;
+//        int count = 0;
+//
+//        for (Payment payment : payments) {
+//            fee.plus(payment.getFee());
+//
+//            if (++count > TARGET_DISCOUNT_FREQUENCY) {
+//                fee.minus(discountAmount);
+//            }
+//        }
+//
+//        return fee;
+//    }
 
 }
