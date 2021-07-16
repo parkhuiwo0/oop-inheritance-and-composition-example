9c051b072c0c412add70767bbc9d61f96682d18b
휴/박희우 <parkhuiwo0@gmail.com>
2021-07-16 10:11:46 +0900

    중복 코드를 부모 클래스로 올려라.

--- src/domain/card/Card.java
+++ src/domain/card/Card.java
@@ -6,33 +6,19 @@ import domain.payment.Payment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Card {
+public abstract class Card {
+
     private List<Payment> payments = new ArrayList<>();
 
-    public Card(List<Payment> payments) {
-        this.payments = payments;
-    }
-
-    public List<Payment> getPayments() {
-        return payments;
-    }
-
-    /**
-     * 결제 내역을 순회 하면서 금액을 더합니다.
-     * @return 결제 내역의 총 금액 합산
-     */
     public Money calculateFee() {
         Money fee = Money.ZERO;
 
         for (Payment payment : payments) {
-            fee.plus(calculatePaymentFee(payment));
+            fee = fee.plus(calculatePaymentFee(payment));
         }
 
         return fee;
     }
 
-    private Money calculatePaymentFee(Payment payment) {
-        return payment.getFee();
-    }
-
+    abstract protected Money calculatePaymentFee(Payment payment);
 }
